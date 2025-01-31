ef37b3030f1fdc18eee459e93bef9500d4e5dfe9
Ansel-Grimm <agrimm28@students.stab.org>
2025-01-31 10:41:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var providers = getColumn(url,1)
 var locations = getColumn(url,2)
 var accessLocations = getColumn(url,5)
 var boroughs = getColumn(url,6)
+var neighborhoods = getColumn(url,7)
+
 
 var uniqueProviders = [];
 for(var i=0; i < providers.length; i++){
@@ -14,4 +16,30 @@ for(var i=0; i < providers.length; i++){
         // it adds it to the list
     }
 }
-console.log(uniqueProviders)
+
+var internetSelect = document.getElementById("internetProvides")
+for (var i = 0; i < uniqueProviders.length; i++){
+    var el = document.createElement("option")
+    el.innerHTML = uniqueProviders[i];
+    el.value = uniqueProviders[i];
+    internetSelect.appendChild(el);
+}
+
+
+var uniqueNeighborhoods = []
+for(var i=0; i < neighborhoods.length; i++){
+    // goes through the list of parties
+    if(!uniqueNeighborhoods.includes(neighborhoods[i])){
+        // when there is a new one
+        uniqueNeighborhoods.push(neighborhoods[i])
+        // it adds it to the list
+    }
+}
+
+var neighborhoodSelect = document.getElementById("Neighborhoods")
+for (var i = 0; i < uniqueNeighborhoods.length; i++){
+    var el2 = document.createElement("option")
+    el2.innerHTML = uniqueNeighborhoods[i];
+    el2.value = uniqueNeighborhoods[i];
+    neighborhoodSelect.appendChild(el2);
+}
