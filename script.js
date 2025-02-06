762a754277e57900f8c44f5b50cdb1e1000a6127
Ansel-Grimm <agrimm28@students.stab.org>
2025-02-06 11:57:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-
-
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/NYC%20Public%20Wifi%20Locations.csv"
 var providers = getColumn(url,1)
 var locations = getColumn(url,2)
 var accessLocations = getColumn(url,5)
 var boroughs = getColumn(url,6)
 var neighborhoods = getColumn(url,7)
+// makes the library also an indoor location not just ones including indoor.
 var indoorLocations = ["Library", "Indoor"]
 
 var uniqueProviders = [];
 for(var i=0; i < providers.length; i++){
-    // goes through the list of parties
+    // goes through the list of providers.
     if(!uniqueProviders.includes(providers[i])){
-        // when there is a new one
+        // when there is a new provider.
         uniqueProviders.push(providers[i])
-        // it adds it to the list
+        // it adds it to the list of unique providers.
     }
 }
 uniqueProviders.sort()
+// puts the list of providers into alphabetical order.
+
 var internetSelect = document.getElementById("internetProvides")
 for (var i = 0; i < uniqueProviders.length; i++){
     var el = document.createElement("option")
@@ -25,19 +26,21 @@ for (var i = 0; i < uniqueProviders.length; i++){
     el.value = uniqueProviders[i];
     internetSelect.appendChild(el);
 }
+// takes the list of unique internet providers and makes a drop down from them
 
 
 var uniqueNeighborhoods = []
 for(var i=0; i < neighborhoods.length; i++){
-    // goes through the list of parties
+    // goes through the list of neighborhoods
     if(!uniqueNeighborhoods.includes(neighborhoods[i])){
-        // when there is a new one
+        // when there is a new neighborhoods
         uniqueNeighborhoods.push(neighborhoods[i])
-        // it adds it to the list
+        // it adds it to the list unique neighborhoods
     }
 }
 
 uniqueNeighborhoods.sort()
+// sorts the neighborhoods into alphabetical order
 
 var neighborhoodSelect = document.getElementById("Neighborhoods")
 for (var i = 0; i < uniqueNeighborhoods.length; i++){
@@ -46,7 +49,16 @@ for (var i = 0; i < uniqueNeighborhoods.length; i++){
     el2.value = uniqueNeighborhoods[i];
     neighborhoodSelect.appendChild(el2);
 }
+// takes the list of unique neighborhoods and makes a drop down from them
 
+
+
+
+// findPublicWIFI has three peramitaers that are the three inputs in the dropdowns.
+// then it takes those 3 inputs and checks to see if their are any matches for them.
+// if there is it will take that list of matches and make it into a string,
+// it will also switch to another page where it will desplay the resluts.
+// if there are no matches found then it brings you to a differnt page to say that
 function findPublicWIFI(providerInput, neighborhoodInput, locationInput){
     console.log(providerInput)
     console.log(neighborhoodInput)
@@ -84,18 +96,13 @@ function findPublicWIFI(providerInput, neighborhoodInput, locationInput){
         
     }
 }
-
+// takes the string from sessionStorage and gives it an id 
 function getData(){
     var matches = JSON.parse(sessionStorage.getItem("matches"));
+    
+    document.getElementById("matches").innerHTML = matches.join("<br>")}
 
-   document.getElementById("matches").innerHTML= matches;
-
-}
-
-
+// when this button is clicked on a  differnt page you are sent to the main html page
 function goBack(){
     location.replace("indext.html");
-  }
-function pullResults(){
-    
 }
